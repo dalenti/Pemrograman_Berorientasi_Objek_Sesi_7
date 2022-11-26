600c208cf5d6127b37ef2594e2f26bfda41359ee
dalenti <114300375+dalenti@users.noreply.github.com>
2022-11-26 14:33:34 +0700

    Add files via upload

---
+++ Outer2.java
@@ -0,0 +1,25 @@
+package no2;
+
+public class Outer2 {
+    int number = 0;
+
+    void outerMethod(){
+        class Inner{
+            public void print(){
+                System.out.println("mengakses inner class yang ke-: " + (number++));
+            }
+        }
+        Inner inner = new Inner(); 
+        inner.print();
+    }
+}
+
+class OuterAcces{
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+
+        outer.outerMethod();
+        outer.outerMethod();
+        outer.outerMethod();
+    }
+}
